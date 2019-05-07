e70c80f6156282a77690dc3af1a19a968ce14b98
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2019-05-08 01:35:28 +0200

    Fixed code when creating a new user.

--- users/models.go
+++ users/models.go
@@ -22,7 +22,7 @@ type User struct {
 
 func New(db *sql.DB, username string, email string, password string) User {
 	var user User
-	qry := `insert into users (username, email, password) values ($1, $2, $3) returning *`
+	qry := `insert into users (username, email, password) values ($1, $2, $3) returning id, username, email, joined_at`
 	res := db.QueryRow(qry, username, email, HashPassword(password))
 	err := res.Scan(&user.Id, &user.Username, &user.Email, &user.JoinedAt)
 	if err != nil {
